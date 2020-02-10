ee928e51ee47621ef2ad8638ff490cad446d76ad
Abhishek Samariya <asamariya@gmail.com>
2020-02-10 15:49:36 +1100

    Added Gsap timelines

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,56 @@
-const headers = document.querySelectorAll('h2, h3');
-const imageHolders = document.querySelectorAll('.image');
+const runScripts = () => {
+  const headers = document.querySelectorAll('h2, h3');
+  const imageHolders = document.querySelectorAll('.image');
 
-const toggleInView = entries => {
-  entries.forEach(entry => {
-    if (entry.intersectionRatio >= 0.1) {
-      entry.target.classList.add('in-view');
-    } else {
-      entry.target.classList.remove('in-view');
-    }
-  });
-};
-const options = {
-  threshold: [0, 0.1, 1]
-};
-const observer = new IntersectionObserver(toggleInView, options);
+  const toggleInView = entries => {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio >= 0.1) {
+        entry.target.classList.add('in-view');
+      } else {
+        entry.target.classList.remove('in-view');
+      }
+    });
+  };
+  const options = {
+    threshold: [0, 0.1, 1]
+  };
+  const observer = new IntersectionObserver(toggleInView, options);
 
-headers.forEach(header => observer.observe(header));
-imageHolders.forEach(holder => observer.observe(holder));
+  headers.forEach(header => observer.observe(header));
+  imageHolders.forEach(holder => observer.observe(holder));
+};
+
+runScripts();
 
 barba.init({
-  transitions: [],
+  transitions: [
+    {
+      name: 'switch',
+      leave: ({ current, next, trigger }) => {
+        return new Promise(resolve => {
+          const timeline = gsap.timeline({
+            onComplete: () => {
+              resolve();
+            }
+          });
+
+          timeline.to('header', { y: '-100%' });
+        });
+      },
+      enter: ({ current, next, trigger }) => {
+        return new Promise(resolve => {
+          const timeline = gsap.timeline({
+            onComplete: () => {
+              runScripts();
+              resolve();
+            }
+          });
+
+          timeline.to('header', { y: '0%' });
+        });
+      }
+    }
+  ],
   views: [],
   debug: true
 });
